f5ac4935c1277a7b4051eb2e4661ec5a16d50bdb
sena <gulozseven@gmail.com>
2025-11-26 00:49:33 +0300

    gitignore

--- AppWeb.Web/obj/Debug/net9.0/AppWeb.Web.AssemblyInfo.cs
+++ AppWeb.Web/obj/Debug/net9.0/AppWeb.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppWeb.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5dca376b1cd5b2569294b1ff4d8fef8fb615d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3385d55405562f5add4b689b82e94f9adec07e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppWeb.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppWeb.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
