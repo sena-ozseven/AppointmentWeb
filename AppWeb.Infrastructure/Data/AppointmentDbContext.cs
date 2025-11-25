2c7d4c49ef56e04532a918c338cbae643f09173e
sena <gulozseven@gmail.com>
2025-11-25 23:43:20 +0300

    Update AppointmentDbContext.cs

--- AppWeb.Infrastructure/Data/AppointmentDbContext.cs
+++ AppWeb.Infrastructure/Data/AppointmentDbContext.cs
@@ -2,13 +2,17 @@ using Microsoft.EntityFrameworkCore;
 using AppWeb.Domain.Entities;
 
 namespace AppWeb.Infrastructure.Data;
-public class AppointmentDbContext : DbContext
+public class AppointmentDbContext : DbContext //DbContext: EF Core'dan INHERIT olan sınıf. Db ile konuşan SERVICE; ORM SESSION
 {
+    //DB CONFIGURATION
+    //constructor --> EF Core'u bağlar, migration yapar, DI
     public AppointmentDbContext(DbContextOptions<AppointmentDbContext> options) 
-        : base(options)
+        : base(options) //java'da --> super(config) parent'a config gönderiyoruz.
     {
     }
 
+
+//database'de tablo oluşturma satırları:
     public DbSet<Appointment> Appointments => Set<Appointment>();
     public DbSet<Branch> Branches => Set<Branch>();
     public DbSet<AppointmentStatusHistory> AppointmentStatusHistories => Set<AppointmentStatusHistory>();
