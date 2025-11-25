5b19f9b6da48c29a47c7329fc74a41173d62f36a
sena <gulozseven@gmail.com>
2025-11-25 22:47:34 +0300

    Update AppointmentDbContext.cs

--- AppWeb.Infrastructure/Data/AppointmentDbContext.cs
+++ AppWeb.Infrastructure/Data/AppointmentDbContext.cs
@@ -1,5 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using AppWeb.Domain.Entities;
 
-public class AppointmentDbContext
+namespace AppWeb.Infrastructure.Data;
+public class AppointmentDbContext : DbContext
 {
-    
+    public AppointmentDbContext(DbContextOptions<AppointmentDbContext> options) 
+        : base(options)
+    {
+    }
+
+    public DbSet<Appointment> Appointments => Set<Appointment>();
+    public DbSet<Branch> Branches => Set<Branch>();
+    public DbSet<AppointmentStatusHistory> AppointmentStatusHistories => Set<AppointmentStatusHistory>();
 }
